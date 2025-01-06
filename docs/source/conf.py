c6b52bcaecc83c6430901d92d75183667fde9aba
ruthvik07 <ruthvik.koppala@gmail.com>
2025-01-06 14:57:51 +0100

    notebook width changed to 70%

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-sphinx_rtd_size_width = "90%"
+sphinx_rtd_size_width = "70%"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'    
