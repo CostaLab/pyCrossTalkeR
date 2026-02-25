d0f1066f337c8e4d4c092950eae3591954e2936f
jsnagai <james.nagai@gmail.com>
2026-02-26 00:15:28 +0100

    adding gmean

--- pycrosstalker/tools/utils.py
+++ pycrosstalker/tools/utils.py
@@ -4,7 +4,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import igraph
 import itertools
-from scipy.stats import fisher_exact, MonteCarloMethod, mannwhitneyu
+from scipy.stats import fisher_exact, MonteCarloMethod, mannwhitneyu, gmean
 from scipy.cluster.hierarchy import linkage, leaves_list
 import pickle
 from itertools import combinations
